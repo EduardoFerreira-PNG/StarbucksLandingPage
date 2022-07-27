95b8cd06d3baef8ea40bb87129ea32f0cd049f25
Eduardo <eduardonaves02@outlook.com>
2022-07-27 17:12:53 -0300

    Finalização da landing page do Starbucks

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function imgSlider(valor){
+    document.querySelector(".img__produto").src = valor;
+}
+
+function mudarCorDoFundo(cor){
+    const produto = document.querySelector(".produto");
+    produto.style.background = cor;
+}
+
+
